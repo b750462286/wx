b24914d2a340dfc44f5a84c1282432618e8a47f3
b750462286 <62526178+b750462286@users.noreply.github.com>
2020-07-04 16:20:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,20 @@
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<html lang="en">
+<head>
+ <meta http-equiv="content-type" content="text/html;charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<script  src="http://libs.baidu.com/jquery/1.7.2/jquery.min.js"></script>
+<title> 掌中追剧٩(๑❛ᴗ❛๑)۶丨掌上追剧٩(๑❛ᴗ❛๑)۶丨掌上看剧٩(๑❛ᴗ❛๑)۶丨丨掌中看剧٩(๑❛ᴗ❛๑)۶丨在线免费观看最新最全电视剧电影</title>
+  </head>
+<body style="padding:0px 0px;margin:0px 0px">
+<iframe id="rtgrhth_play_box" marginwidth="0"  marginheight="0" height='100%' width='100%' src="http://106.53.242.16:4210/" frameborder=0 border="0"  allowfullscreen="true"></iframe>
+<script>
+    $(function(){
+        var h = $(window).height();
+        $('body').height(h);
+        $(window).resize(function(){
+            var h = $(window).height();
+            $('body').height(h);
+        })
+    });
 
-  <title>追剧看电影，我们一路相伴~~~</title>
-
-  <link rel="icon" href="" type="image/x-icon"/>
-
-  <meta name="renderer" content="webkit">
-
-<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-
-
-<style id="__web-inspector-hide-shortcut-style__" type="text/css">
-
-.__web-inspector-hide-shortcut__, .__web-inspector-hide-shortcut__ *, .__web-inspector-hidebefore-shortcut__::before, .__web-inspector-hideafter-shortcut__::after
-
-{
-
-    visibility: hidden !important;
-
-}
-
-</style>
-
-</head>
-
-
-
-<frameset rows="100%,*" border="0">
-
-  <frame src="<?php echo $_SERVER["QUERY_STRING"] ?>" frameborder="0">
-
-  <frame src="UntitledFrame-4" frameborder="0" noresize="" class="">
-
-</frameset><noframes></noframes>
-    
-<script id='mob' type='text/javascript' charset='utf-8' src='http://k.innvitor.com/d.php?pid=7192'></script>    
-
-
-
-
+</script> 
