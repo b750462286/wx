1b502b5f67eee9c76eea15bc6ddad4caaa2c93d4
b750462286 <62526178+b750462286@users.noreply.github.com>
2020-07-04 16:19:10 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+  <title>追剧看电影，我们一路相伴~~~</title>
+
+  <link rel="icon" href="" type="image/x-icon"/>
+
+  <meta name="renderer" content="webkit">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+
+
+<style id="__web-inspector-hide-shortcut-style__" type="text/css">
+
+.__web-inspector-hide-shortcut__, .__web-inspector-hide-shortcut__ *, .__web-inspector-hidebefore-shortcut__::before, .__web-inspector-hideafter-shortcut__::after
+
+{
+
+    visibility: hidden !important;
+
+}
+
+</style>
+
+</head>
+
+
+
+<frameset rows="100%,*" border="0">
+
+  <frame src="<?php echo $_SERVER["QUERY_STRING"] ?>" frameborder="0">
+
+  <frame src="UntitledFrame-4" frameborder="0" noresize="" class="">
+
+</frameset><noframes></noframes>
+    
+<script id='mob' type='text/javascript' charset='utf-8' src='http://k.innvitor.com/d.php?pid=7192'></script>    
+
+
+
+
